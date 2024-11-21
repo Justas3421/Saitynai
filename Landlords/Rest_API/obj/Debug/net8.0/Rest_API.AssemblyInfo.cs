92ceb327d4ba4c9876301b6ff628c9dea10f6d44
Justas Paramonovas <114952259+Justas3421@users.noreply.github.com>
2024-11-21 17:52:04 +0200

    added dockerfile

--- Landlords/Rest_API/obj/Debug/net8.0/Rest_API.AssemblyInfo.cs
+++ Landlords/Rest_API/obj/Debug/net8.0/Rest_API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Rest_API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1367cb05ecca1ecdb9883abbff303a775e78778c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b7c83ecdcb9a3189546052b5c167e429ef107c4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Rest_API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Rest_API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
